a8cf4c27beba4e11aebb20c1a87cd13bc6550fab
Meldanor <Bhaals@gmx.de>
2012-08-31 20:10:11 +0200

    Add command structure
    Signed-off-by: Meldanor <Bhaals@gmx.de>

---
+++ src/main/java/de/minestar/contao/commands/contao/cmdContao.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2012 MineStar.de 
+ * 
+ * This file is part of Contao.
+ * 
+ * Contao is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * Contao is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Contao.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.minestar.contao.commands.contao;
+
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+
+import de.minestar.contao.core.ContaoCore;
+import de.minestar.minestarlibrary.commands.AbstractCommand;
+import de.minestar.minestarlibrary.commands.AbstractSuperCommand;
+
+public class cmdContao extends AbstractSuperCommand {
+
+    public cmdContao(String syntax, String arguments, String node, AbstractCommand... subCommands) {
+        super(ContaoCore.NAME, syntax, arguments, node, subCommands);
+    }
+
+    @Override
+    public void execute(String[] args, Player player) {
+        // DO NOTHING - SUPER COMMAND
+    }
+
+    @Override
+    public void execute(String[] args, ConsoleCommandSender console) {
+        // DO NOTHING - SUPER COMMAND
+    }
+}
--- src/main/java/de/minestar/contao/core/ContaoCore.java
+++ src/main/java/de/minestar/contao/core/ContaoCore.java
@@ -2,8 +2,26 @@ package de.minestar.contao.core;
 
 import java.io.File;
 
+import de.minestar.contao.commands.contao.cmdContao;
+import de.minestar.contao.commands.contao.cmdFreeSlot;
+import de.minestar.contao.commands.list.cmdList;
+import de.minestar.contao.commands.user.cmdAddProbeTime;
+import de.minestar.contao.commands.user.cmdAddWarning;
+import de.minestar.contao.commands.user.cmdAdmin;
+import de.minestar.contao.commands.user.cmdDefault;
+import de.minestar.contao.commands.user.cmdFree;
+import de.minestar.contao.commands.user.cmdMod;
+import de.minestar.contao.commands.user.cmdPay;
+import de.minestar.contao.commands.user.cmdProbe;
+import de.minestar.contao.commands.user.cmdRemoveWarning;
+import de.minestar.contao.commands.user.cmdSearch;
+import de.minestar.contao.commands.user.cmdStatus;
+import de.minestar.contao.commands.user.cmdUnMod;
+import de.minestar.contao.commands.user.cmdUser;
+import de.minestar.contao.commands.user.cmdX;
 import de.minestar.contao.database.DatabaseHandler;
 import de.minestar.minestarlibrary.AbstractCore;
+import de.minestar.minestarlibrary.commands.CommandList;
 
 public class ContaoCore extends AbstractCore {
 
@@ -12,12 +30,67 @@ public class ContaoCore extends AbstractCore {
     // DATABASE HANDLER
     public static DatabaseHandler dbHandler;
 
+    public ContaoCore() {
+        super(NAME);
+    }
+
     @Override
     protected boolean createManager() {
 
         dbHandler = new DatabaseHandler(NAME, new File(getDataFolder(), "sqlconfig.yml"));
         if (dbHandler == null || !dbHandler.hasConnection())
             return false;
+
+        return true;
+    }
+
+    @Override
+    protected boolean createCommands() {
+        // @formatter:off
+        
+        cmdList = new CommandList(NAME, 
+
+                // LIST COMMAND
+                new cmdList         ("/who",        "",     ""),
+                new cmdList         ("/list",       "",     ""),
+                new cmdList         ("/online",     "",     ""),
+
+                new cmdStatus       ("/stats",      "",     ""),
+
+                // USER-MANGEMENT COMMANDS
+                new cmdUser         ("/user",       "",     "",
+                        
+                        // GROUP CHANGE COMMANDS
+                        new cmdX            ("x",           "<IngameName> <Grund>",         "contao.rights.x"),
+                        new cmdDefault      ("default",     "<IngameName>",                 "contao.rights.default"),
+                        new cmdProbe        ("probe",       "<IngameName> <ContaoID>",      "contao.rights.probe"),
+                        new cmdFree         ("free",        "<IngameName>",                 "contao.rights.free"),
+                        new cmdPay          ("pay",         "<IngameName> <DD.MM.YYYY>",    "contao.rights.pay"),
+                        new cmdMod          ("mod",         "<IngameName> ",                "contao.rights.mod"),
+                        new cmdUnMod        ("unmod",       "<IngameName> ",                "contao.rights.unmod"),
+                        new cmdAdmin        ("admin",       "<IngameName> <DD.MM.YYYY>",    "contao.rights.admin"),
+
+                        // USEFUL COMMANDS
+                        new cmdSearch       ("search",      "<HomePage Name>",              "contao.rights.search"),
+                        new cmdStatus       ("status",      "",                             ""),
+
+                        // PUNISHMENT COMMANDS
+                        new cmdAddWarning   ("awarn",       "<IngameName> <Text>",          "contao.rights.awarn"),
+                        new cmdRemoveWarning("rwarn",       "<IngameName> <Index>",         "contao.rights.rwarn"),
+
+                        new cmdAddProbeTime ("probeadd",    "<IngameName> <Tage>",          "contao.rights.probeadd")
+
+                ),
+
+                // CONTAO MANAGEMENT COMMANDS
+                new cmdContao       ("/contao",     "",     "",
+
+                        new cmdFreeSlot     ("freeslots",   "<Zahl>",                       "contao.rights.freeslots")
+
+                )        
+
+        );
+        // @formatter:on
 
         return true;
     }
