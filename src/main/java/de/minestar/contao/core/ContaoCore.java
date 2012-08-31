4aee701317bc281ac535e6b248689835319f5501
Meldanor <Bhaals@gmx.de>
2012-08-31 10:48:24 +0200

    Initial commit
    Signed-off-by: Meldanor <Bhaals@gmx.de>

---
+++ src/main/java/de/minestar/contao/core/ContaoCore.java
@@ -0,0 +1,9 @@
+package de.minestar.contao.core;
+
+import de.minestar.minestarlibrary.AbstractCore;
+
+public class ContaoCore extends AbstractCore {
+
+    public static final String NAME = "Contao";
+
+}
